aaa50d847171257d0510e4f483f895c300715cd6
Dan Gialdini <dangialdini@gmail.com>
2023-03-12 10:18:10 +1100

    Update about page

--- scripts/router.js
+++ scripts/router.js
@@ -31,7 +31,7 @@ Router.prototype = {
         if (window.location.hash.length > 0) {
             for (var i = 0, length = r.length; i < length; i++) {
                 var route = r[i];
-                if (route.isActiveRoute(window.location.hash.substr(1))) {
+                if (route.isActiveRoute(window.location.hash.substring(1))) {
                     scope.goToRoute(route.htmlName);
                 }
             }
